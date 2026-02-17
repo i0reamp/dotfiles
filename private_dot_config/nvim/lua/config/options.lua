4f0567a525cb29915e02ceef1d071105670c3db0
Alexander Kotov <kot@amicon.ru>
2026-02-17 12:59:16 +0300

    Initial commit

---
+++ private_dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.o.fileencodings = "utf-8,cp866,cp1251"
+
+vim.g.snacks_animate = false
+vim.g.autoformat = false
+
+vim.o.tabstop = 8 -- A TAB character looks like 4 spaces
---
+++ private_dot_config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,28 @@
+return {
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        bashls = {
+          filetypes = { "sh", "zsh" },
+        },
+      },
+    },
+  },
+
+  -- add any tools you want to have installed below
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "hadolint",
+      },
+    },
+  },
+}
